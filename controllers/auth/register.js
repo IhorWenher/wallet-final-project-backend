2f3d848f4f1eeeac35f63367123180396bccee64
Andrew-Romanov <andrewromanov@yahoo.com>
2021-11-16 16:13:23 +0200

    Added functional for authentication and balance

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,24 +1,29 @@
-const { Conflict } = require("http-errors");
+const { Conflict, InternalServerError } = require('http-errors')
+const { User } = require('../../models')
 
-const { User } = require("../../models");
+const register = async (req, res, next) => {
+  const { name, email, password } = req.body
+  const user = await User.findOne({ email })
 
-const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    throw new Conflict("Already registered");
-  }
+  if (user) throw new Conflict('Email in use')
 
-  const newUser = new User({ email, name });
-  newUser.setPassword(password);
+  const newUser = new User({ name, email })
+  newUser.setPassword(password)
+  const userData = await newUser.save()
 
-  await newUser.save();
+  if (!userData) throw new InternalServerError('Server error')
 
   res.status(201).json({
-    status: "success",
+    status: 'User registered',
     code: 201,
-    message: "Register success",
-  });
-};
+    data: {
+      user: {
+        name: userData.name,
+        email: userData.email,
+        balance: userData.balance
+      }
+    }
+  })
+}
 
-module.exports = register;
+module.exports = register
--- controllers/users/updateBalance.js
+++ controllers/users/updateBalance.js
@@ -1,5 +1,23 @@
-const { User } = require("../../models");
+const { InternalServerError } = require('http-errors')
+const { User } = require('../../models')
 
-const updateBalance = async () => {};
+const updateBalance = async (req, res, next) => {
+  const { balance } = req.body
 
-module.exports = updateBalance;
+  const userData = await User
+    .findByIdAndUpdate(req.user._id, { balance }, { new: true })
+
+  if (!userData) throw new InternalServerError('Server error')
+
+  res.status(200).json({
+    status: 'Balance updated',
+    code: 200,
+    data: {
+      user: {
+        balance: userData.balance
+      }
+    }
+  })
+}
+
+module.exports = updateBalance
