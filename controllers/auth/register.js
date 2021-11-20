617a4c1c8427c008180e4e118be2c9b6a74261a1
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-11-20 11:17:41 +0200

    added_token_on_register

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,29 +1,47 @@
-const { Conflict, InternalServerError } = require('http-errors')
-const { User } = require('../../models')
+const { Conflict, InternalServerError } = require("http-errors");
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
-  const { name, email, password } = req.body
-  const user = await User.findOne({ email })
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
 
-  if (user) throw new Conflict('Email in use')
+  if (user) throw new Conflict("Email in use");
 
-  const newUser = new User({ name, email })
-  newUser.setPassword(password)
-  const userData = await newUser.save()
+  const newUser = new User({ name, email });
+  newUser.setPassword(password);
 
-  if (!userData) throw new InternalServerError('Server error')
+  const userData = await newUser.save();
+
+  if (!userData) throw new InternalServerError("Server error");
+
+  let newUserData = await User.findOne({ email });
+
+  const payload = { id: newUserData._id };
+  const token = jwt.sign(payload, SECRET_KEY);
+
+  newUserData = await User.findByIdAndUpdate(
+    newUserData._id,
+    { token },
+    { new: true }
+  );
+
+  if (!newUserData) throw new InternalServerError("Server error");
 
   res.status(201).json({
-    status: 'User registered',
+    status: "User registered",
     code: 201,
     data: {
+      token,
       user: {
-        name: userData.name,
-        email: userData.email,
-        balance: userData.balance
-      }
-    }
-  })
-}
+        name: newUserData.name,
+        email: newUserData.email,
+        balance: newUserData.balance,
+      },
+    },
+  });
+};
 
-module.exports = register
+module.exports = register;
