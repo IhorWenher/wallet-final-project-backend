474d51644e3dcde49f73968edce58d4a9d800838
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-11-21 18:47:40 +0200

    updated_balance

--- controllers/user-transactions/add.js
+++ controllers/user-transactions/add.js
@@ -3,30 +3,31 @@ const { Transaction } = require("../../models");
 const { User } = require("../../models");
 
 const add = async (req, res) => {
-  const newTransaction = { ...req.body, owner: req.user._id };
-
-  let transactionData = await Transaction.create(newTransaction);
-
-  if (!transactionData) throw new InternalServerError("Server error");
-
   const userData = await User.findById(req.user._id);
 
   let newBalance = 0;
+
   transactionData.type
     ? (newBalance = userData.balance + req.body.sum)
     : (newBalance = userData.balance - req.body.sum);
 
   if (newBalance < 0) throw new BadRequest("The balance cannot be less than 0");
 
-  console.log(newBalance);
+  const newTransaction = {
+    ...req.body,
+    balance: newBalance,
+    owner: req.user._id,
+  };
+
+  let transactionData = await Transaction.create(newTransaction);
+
+  if (!transactionData) throw new InternalServerError("Server error");
 
   const newUserData = await User.findByIdAndUpdate(
     req.user._id,
     { balance: newBalance },
     { new: true }
   );
-
-  console.log(newUserData);
 
   if (!newUserData) throw new InternalServerError("Server error");
 
@@ -39,7 +40,7 @@ const add = async (req, res) => {
     category: transactionData.category,
     comment: transactionData.comment ? transactionData.comment : "",
     sum: transactionData.sum,
-    balance: newUserData.balance,
+    balance: transactionData.balance,
   };
 
   res.status(201).json({
